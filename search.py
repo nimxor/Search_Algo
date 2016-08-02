cd82d96be1070d27b2feb44aebfe507c5045c61f
Your Name <shubhamgupta190496@gmail.com>
2016-08-03 01:24:04 +0530

    search.py

--- search.py
+++ search.py
@@ -12,17 +12,24 @@ def star():
 	cols = int(cols)
 	print( '\n' +  '*'*cols + '\n' )
 
+doc = {}
+
 with open('where.json') as data_file:    
     fhand = json.load(data_file)
     for line in fhand :
     	title = line[0]['title']
     	star()
     	print title
+    	web_list = []
     	star()
     	for data in line :
     		data = str(data).strip().split(':')
     		if data[0]=="{u'title'" :
     			continue
-    		data = str(data).split("u'")
-    		print data
-    	
+    		link = data[1]+data[2]
+    		web_title = data[0][2:]
+    		web_link = link[2:-1]
+    		web_list.append([web_title,web_link])
+    	doc[title]=web_list
+    		
+print doc    	
